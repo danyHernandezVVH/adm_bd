8c41783ebcff8df934182e2e50c5a8bcbf86678f
danyHernandezVVH <danyhernandez@comeduc.cl~>
2025-03-10 12:25:20 -0300

    ok

--- 01_Ejercicio_BD/script prueba.sql
+++ 01_Ejercicio_BD/script prueba.sql
@@ -4,3 +4,7 @@ use prueba;
 
 create table usuario(id INT, username VARCHAR(50),
 password varchar(50));
+
+
+  git config --global user.email "danyhernandez@comeduc.cl"
+  git config --global user.name "danyHernandezVVH"
